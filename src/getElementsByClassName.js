05a6baed587454a727eee4980f6187157b2f9aab
joksina <jksalaudeen@yahoo.com>
2015-10-19 13:06:11 -0700

    matches the result of built-in function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  //create an empty array
+  var list = [];
+  //creat an anonymous function to check the list
+  function checkList(element){
+  //check classes and push value to list
+  if(element.classList && element.classList.contains(className)){
+  list.push(element);
+  }
+  //iterate through childnode
+  _.each(element.childNodes, function(value){
+  //return recursed value
+  return checkList(value);
+  });
+ }
+ //get all the element in the body
+  checkList(document.body);
+
+  return list;
 };
+

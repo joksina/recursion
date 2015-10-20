4131a411fc9d0d8fd0c3af0904f377e0eea9248c
joksina <jksalaudeen@yahoo.com>
2015-10-20 12:59:04 -0700

    stringyfy done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result;
+  //check if the obj is null
+ if (obj === null) {
+    return "" + null + "";
+  }else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }else if (typeof obj === "boolean" || typeof obj === "number"){
+    return obj.toString();
+  }
+  //check if the object is an array
+if (Array.isArray(obj)) {
+ result = [];
+	_.each(obj, function(val){
+      result.push(stringifyJSON(val));
+    });
+    return '[' + result + ']';
+}
+
+if (typeof(obj) === "object"){
+ result = [];
+   _.each(obj, function(value, key){
+      if (value === undefined || typeof(value) === "function") {
+        return '{}';
+      }
+    result.push((stringifyJSON(key) + ":" + stringifyJSON(value)));
+    });
+    return '{' + result + '}';
+  }
+  return "" + obj + "";
 };
